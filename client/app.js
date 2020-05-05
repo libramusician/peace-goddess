f9a08da4494d1939d9df6c850d265fead9262cc7
han-never-solo <hanqiuzh@gmail.com>
2020-05-05 17:58:00 -0400

    refactor for multi-tanks & different ai strategies

--- client/app.js
+++ client/app.js
@@ -1,12 +1,79 @@
 const PIXI = require('pixi.js');
+const UserAgent = require('./game/agent/userAgent')
+const RandomAgent = require('./game/agent/randomAgent')
 const keyboard = require('./key').keyboard;
+const gameWidth = 512
+const gameHeight = 512
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container
 const app = new PIXI.Application()
 app.renderer.autoResize = true
 app.renderer.resize(512,512)
-let tank;
+let tanks = [];
+let bullets = [];
+const bulletSpeed = 10
+const actions = [
+'LEFT_UP','UP','RIGHT_UP',
+'LEFT','','RIGHT',
+'LEFT_DOWN','DOWN','RIGHT_DOWN',
+'LEFT_UP_FIRE','UP_FIRE','RIGHT_UP_FIRE',
+'LEFT_FIRE','FIRE','RIGHT_FIRE',
+'LEFT_DOWN_FIRE','DOWN_FIRE','RIGHT_DOWN_FIRE'
+]
+
+const createTank = (sprite,agent,x,y)=>{
+  let tank = sprite
+  tank.agent = agent
+  tank.vx = 0
+  tank.vy = 0
+  tank.direction = 'U'
+  tank.fireBullet = ()=>{
+    let circle = new PIXI.Graphics()
+    circle.beginFill(0xFFCC00, 1)
+    circle.drawCircle(2,2,2)
+    circle.endFill()
+    temp = new PIXI.Sprite(app.renderer.generateTexture(circle))
+    console.log('anchor offset',temp.width,temp.height)
+
+    temp.x=tank.x
+    temp.y=tank.y
+    switch(tank.direction){
+      case 'U': 
+        temp.vy = -bulletSpeed;
+        temp.vx = 0;
+        temp.y -= tank.height/2
+        temp.x -= tank.width/16 //adjust position
+        break;
+      case 'L': 
+        temp.vx = -bulletSpeed;
+        temp.vy = 0;
+        temp.x -= tank.width/2
+        break;
+      case 'D': 
+        temp.vy = bulletSpeed;
+        temp.vx = 0;
+        temp.y += tank.height/2
+        break;
+      case 'R': 
+        temp.vx = bulletSpeed;
+        temp.vy = 0;
+        temp.x += tank.width/2
+        temp.y -= tank.height/16 //adjust position
+        break;
+      default:
+        console.log('wrong dir')
+    }
+    console.log(temp.x,temp.y)
+    bullets.push(temp)
+    app.stage.addChild(temp)
+  }
+  tank.anchor.x = 0.5
+  tank.anchor.y = 0.5
+  tank.x = x? x : 256
+  tank.y = y? y : 256
+  return tank
+}
 
 
 //load resources
@@ -16,114 +83,16 @@ app.loader.add('tankImage','/images/tank.png').load(setup)
 document.querySelector('#game').appendChild(app.view)
 
 let state 
-let bullets = []
 function setup() {
-
-  //Create the `cat` sprite 
-  tank = new PIXI.Sprite(app.loader.resources["tankImage"].texture);
-  tank.anchor.y = 0.5; 
-  tank.anchor.x = 0.5
-  tank.vx = 0;
-  tank.vy = 0;
-  tank.x = 250
-  tank.y = 450;
-  let direction = 'U'; 
-  app.stage.addChild(tank);
-
-  //Capture the keyboard arrow keys
-  let left = keyboard("ArrowLeft"),
-      up = keyboard("ArrowUp"),
-      right = keyboard("ArrowRight"),
-      down = keyboard("ArrowDown"),
-      space = keyboard(" ");
-  space.press = () => {
-      
-    let temp = new PIXI.Graphics()
-    temp.beginFill(0xFFCC00, 1)
-    temp.drawCircle(tank.x,tank.y,2)
-    temp.endFill()
-     
-    switch(direction){
-      case 'U': 
-        temp.vy = -2;
-        temp.vx = 0;
-        break;
-      case 'L': 
-        temp.vx = -2;
-        temp.vy = 0;
-        break;
-      case 'D': 
-        temp.vy = 2;
-        temp.vx = 0;
-        break;
-      case 'R': 
-        temp.vx = 2;
-        temp.vy = 0;
-        break;
-    }
-    bullets.push(temp)
-    app.stage.addChild(temp)     
-  }    
-
-  //Left arrow key `press` method
-  left.press = () => {
-    //Change the cat's velocity when the key is pressed
-    tank.vx = -5;
-    tank.vy = 0;
-    tank.rotation=-1.57;
-    direction = 'L';  
-   };
-    
-    
-  
-  //Left arrow key `release` method
-  left.release = () => {
-    //If the left arrow has been released, and the right arrow isn't down,
-    //and the tank isn't moving vertically:
-    //Stop the tank
-    if (!right.isDown && tank.vy === 0) {
-      tank.vx = 0;
-    }
-  };
-
-  //Up
-  up.press = () => {
-    tank.vy = -5;
-    tank.vx = 0;
-    tank.rotation=0; 
-    direction = 'U'; 
-  };
-  up.release = () => {
-    if (!down.isDown && tank.vx === 0) {
-      tank.vy = 0;
-    }
-  };
-
-  //Right
-  right.press = () => {
-    tank.vx = 5;
-    tank.vy = 0;
-    tank.rotation=1.57 
-    direction = 'R'; 
-  };
-  right.release = () => {
-    if (!left.isDown && tank.vy === 0) {
-      tank.vx = 0;
-    }
-  };
-
-  //Down
-  down.press = () => {
-    tank.vy = 5;
-    tank.vx = 0;
-    tank.rotation=3.14
-    direction = 'D'; 
-  };
-  down.release = () => {
-    if (!up.isDown && tank.vx === 0) {
-      tank.vy = 0;
-    }
-  };
+  let player = createTank(
+    new PIXI.Sprite(app.loader.resources["tankImage"].texture),
+    new UserAgent("ArrowLeft","ArrowRight","ArrowUp","ArrowDown"," "))
+  //let robot1 = createTank(new PIXI.Sprite(app.loader.resources["tankImage"].texture),new RandomAgent())
+  tanks.push(player)
+  // load tanks
+  for(let tank of tanks){
+    app.stage.addChild(tank);
+  }
 
   //Set the game state
   state = play;
@@ -133,18 +102,68 @@ function setup() {
 }
 
 function gameLoop(delta){
-
   //Update the current game state:
   state(delta);
 }
 
 function play(delta) {
-  //Use the tank's velocity to make it move
-  tank.x += tank.vx;
-  tank.y += tank.vy;
+  //update tank
+  tanks.forEach((tank)=>{
+    // handle action (four directions)
+    let act = tank.agent.getAction({},{},actions)
+    switch(act.split('_')[0]){
+      case 'LEFT':
+        tank.vx = -5;
+        tank.vy = 0;
+        tank.rotation=-1.57;
+        tank.direction = 'L';  
+        break
+      case 'RIGHT':
+        tank.vx = 5;
+        tank.vy = 0;
+        tank.rotation=1.57 
+        tank.direction = 'R'; 
+        break
+      case 'UP':
+        tank.vy = -5;
+        tank.vx = 0;
+        tank.rotation=0; 
+        tank.direction = 'U'; 
+        break
+      case 'DOWN':
+        tank.vy = 5;
+        tank.vx = 0;
+        tank.rotation=3.14
+        tank.direction = 'D'; 
+        break
+      default:
+        tank.vx = tank.vy = 0;
+    }
+    if(act.endsWith('FIRE')){
+      tank.fireBullet()
+    }
+    let newx = tank.x + tank.vx
+    let newy = tank.y + tank.vy
+    if (newx >=0 && newx <=gameWidth){
+      tank.x = newx
+    }
+    if (newy >=0 && newy <=gameHeight){
+      tank.y = newy
+    }
+  })
   
   bullets.forEach((bullet)=>{
     bullet.y += bullet.vy;
     bullet.x += bullet.vx;
+    if(bullet.x<-5 || bullet.x>gameWidth+5 || bullet.y<-5 || bullet.y>gameHeight+5){
+      bullet.markForRemoval = true
+      console.log(bullet.x,bullet.y)
+    }
   })
+  //remove bullets from stage
+  bullets.filter(bullet=>bullet.markForRemoval ).forEach((bullet)=>{
+    app.stage.removeChild(bullet)
+  })
+  //shorten the list
+  bullets = bullets.filter(bullet=>!bullet.markForRemoval)
 }
